2f0fff0dc9b548d99487b3fc57b7c3ab084cc49b
iamnas <iamnas.eth@gmail.com>
2025-01-25 17:54:29 +0530

    deployed CSAMM

--- contract/script/CSAMM.s.sol
+++ contract/script/CSAMM.s.sol
@@ -7,17 +7,17 @@ import {MockERC20} from "../src/MockERC20.sol";
 
 contract CSAMMScript is Script {
     CSAMM public csamm;
-    MockERC20 public token0;
-    MockERC20 public token1;
+    // MockERC20 public token0;
+    // MockERC20 public token1;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        token0 = new MockERC20("Token0", "TK0");
-        token1 = new MockERC20("Token1", "TK1");
-        csamm = new CSAMM(address(token0), address(token1));
+        // token0 = new MockERC20("Token0", "TK0");
+        // token1 = new MockERC20("Token1", "TK1");
+        csamm = new CSAMM(address(0x27933345CB6bF57F73Fd7455C84dd5aF64A6c16f), address(0xc5Fc1a8EEc97116335F85dc1D473d45932eeFE4D));
 
         vm.stopBroadcast();
     }
